513ad43cb35b05d63db9f6743b3a286f1d1959d1
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-12-13 09:18:07 -0700

    project scaffolding

---
+++ projects/projectsModel.js
@@ -0,0 +1,5 @@
+const db = require('../data/db-config')
+
+module.exports = {
+
+}
---
+++ projects/projectsRouter.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const projects = require('./projectsModel');
+const router = express.Router();
+
+module.exports = router;
